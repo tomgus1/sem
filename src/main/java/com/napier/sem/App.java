c231e02f816e8a4cdd16c1c7dcd04fbb33537bb8
Timothy Graham <tomgus1.2@gmail.com>
2023-02-14 09:20:21 +0000

    Add method to display employee data

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,16 +2,13 @@ package com.napier.sem;
 
 import java.sql.*;
 
-public class App
-{
+public class App {
     //Connection to MySQL database.
     private Connection con = null;
 
     //Method to return and employee by ID
-    public Employee getEmployee(int ID)
-    {
-        try
-        {
+    public Employee getEmployee(int ID) {
+        try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
@@ -23,18 +20,15 @@ public class App
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
             // Check one is returned
-            if (rset.next())
-            {
+            if (rset.next()) {
                 Employee emp = new Employee();
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
                 return emp;
-            }
-            else
+            } else
                 return null;
-        }
-        catch (Exception e) //catching an exception
+        } catch (Exception e) //catching an exception
         {
             System.out.println(e.getMessage());
             System.out.println("Failed to get employee details");
@@ -42,69 +36,76 @@ public class App
         }
     }
 
+    //Method to display Employee data
+    public void displayEmployee(Employee emp) {
+        if (emp != null) {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
+
     //Connect to the MySQL database.
-    public void connect()
-    {
-        try
-        {
+    public void connect() {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         }
         //Catching an exception
-        catch (ClassNotFoundException e)
-        {
+        catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10; //setting a counter to decrease
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
     }
 
     // Disconnect from the MySQL database.
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e) //Catching an exception
+            } catch (Exception e) //Catching an exception
             {
                 System.out.println("Error closing connection to database");
             }
         }
     }
-    public static void main(String[] args)
-    {
-            // Create new Application
-            App a = new App();
-            // Connect to database
-            a.connect();
-            // Disconnect from database
-            a.disconnect();
-        }
+
+    public static void main(String[] args) {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
+
+        // Disconnect from database
+        a.disconnect();
+    }
 }
