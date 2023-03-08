ed4d4e897199d6f6edc0310a3d06c7fe34513874
Max <mxmivlev@gmail.com>
2023-03-08 21:12:04 +0000

    Adding and updating existing classes

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -1,12 +1,80 @@
 package com.napier.sem;
 
-public class City {
+public class City
+{
+    // Default constructor
 
-    public int ID; //ID column
+    public City() {}
 
-    public String name; //city name
-    public String countryCode; // country code
-    public int population; // population
-    public String district;  // district
-    public String country;    //  country
+    // ID
+
+    private int id;
+
+    // City's Name
+
+    private String name;
+
+    // City's Country Code
+
+    private String countryCode;
+
+    // City's District
+
+    private String district;
+
+    // City's Population
+
+    private int population;
+
+    // Updates the City ID
+
+    public void setId(int id) { this.id = id; }
+
+    //gets the City ID
+
+    public int getId() {
+        return id;
+    }
+
+    // refreshed city's Name
+    public void setName(String name) {
+        this.name = name;
+    }
+    //gets city's Name
+
+    public String getName() {
+        return name;
+    }
+
+    // Updates the Country Code
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    // gets the Country Code of the City
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    // Updates the City District
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+    //gets the City District
+
+    public String getDistrict() {
+        return district;
+    }
+
+    // Updates the City Population
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+    // gets the City Population
+    public int getPopulation() {
+        return population;
+    }
 }
---
+++ src/main/java/com/napier/sem/Population.java
@@ -0,0 +1,41 @@
+package com.napier.sem;
+
+public class Population
+{ // constructor
+    public Population() {}
+
+    // Population's name
+
+    private String name;
+
+    // Population County
+    private long populationCountry;
+
+    // People living in cities
+    private long populationCity;
+
+    // Updates the Population Name
+
+
+    public void setName(String name) { this.name = name; }
+
+    //gets the Population Name
+
+    public String getName() { return name; }
+
+    // Updates the Population of people living in Countries
+
+    public void setCountryPopulation(long populationCountry) { this.populationCountry = populationCountry; }
+
+    //gets the Population of people living in Countries
+
+    public long getCountryPopulation() { return populationCountry; }
+
+    // Updates the Population of people living in Cities
+
+    public void setCityPopulation(long populationCity) { this.populationCity = populationCity; }
+
+    //gets the Population of people living in Cities
+
+    public long getCityPopulation() { return populationCity; }
+}
