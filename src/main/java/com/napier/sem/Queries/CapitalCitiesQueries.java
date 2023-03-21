4125651907c061341b7bb75ce6555372d49d4269
Timothy Graham <tomgus1.2@gmail.com>
2023-03-21 16:42:13 +0000

    Add capital city Continent query

--- src/main/java/com/napier/sem/Queries/CapitalCitiesQueries.java
+++ src/main/java/com/napier/sem/Queries/CapitalCitiesQueries.java
@@ -1,7 +1,6 @@
 package com.napier.sem.Queries;
 
 import com.napier.sem.City;
-import com.napier.sem.Country;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -13,16 +12,16 @@ public class CapitalCitiesQueries {
 
     public static void getAllCapitalCityReports(Connection con) {
 
-        //get country data
+        //get capital city data
         List<City> allCapitalCities = getAllCapitalCities(con);
-//        List<Country> capitalCitiesInContinent = getCapitalCitiesInContinent("Europe", allCapitalCities);
-//        List<Country> capitalCitesInRegion = getCapitalCitiesInRegion("Eastern Asia", allCapitalCities);
+        List<City> capitalCitiesInContinent = getCapitalCitiesInContinent(con,"Europe", allCapitalCities);
+//        List<City> capitalCitesInRegion = getCapitalCitiesInRegion(con,"Eastern Asia", allCapitalCities);
 //        List<Country> allCapitalCitiesLimited = getCapitalCitiesLimitedBy(3, allCapitalCities);
 //        List<Country> capitalCitiesInContinentLimited = getCapitalCitiesLimitedBy(3, capitalCitiesInContinent);
 //        List<Country> capitalCitiesInRegionLimited = getCapitalCitiesLimitedBy(3, capitalCitiesInRegion);
 
         //columns format
-        String format = "%-10s %-50s %-20s %-40s %-15s %-15s";
+        String format = "%-40s %-20s";
 
         //report generation
         printReport(
@@ -30,11 +29,11 @@ public class CapitalCitiesQueries {
                 format,
                 allCapitalCities
         );
-//        printReport(
-//                "All the capital cities in a continent organised by largest population to smallest.",
-//                format,
-//                capitalCitiesInContinent
-//        );
+        printReport(
+                "All the capital cities in a continent organised by largest population to smallest.",
+                format,
+                capitalCitiesInContinent
+        );
 //        printReport(
 //                "All the capital cities in a region organised by largest population to smallest.",
 //                format,
@@ -68,59 +67,80 @@ public class CapitalCitiesQueries {
 
                 // Sends the SQL statement to the database.
                 ResultSet rset = stmt.executeQuery(
-//                        "SELECT city.Name, country.Name, city.Population "
-//                                + "FROM city JOIN country ON country.Code=Code "
-//                                + "WHERE ID = Capital "
-//                                + "ORDER BY city.Population DESC "
-                        "SELECT city.Name, country.Name AS 'Country', city.Population "
-                                + "FROM city JOIN country ON city.CountryCode = country.Code "
-                                + "WHERE city.ID = country.Capital "
-                                + "ORDER BY city.Population DESC "
-                                );
+                        "SELECT city.Name, country.Name AS Country, city.Population "
+                        + "FROM country country "
+                        + "LEFT JOIN city city on country.Capital = city.ID "
+                        + "ORDER BY city.Population DESC ");
 
                 while (rset.next()) {
                     //map query result to capitalCity object and add to list
                     City capitalCity = new City();
 
-                    capitalCity.setCountryCode(rset.getString("Code"));
                     capitalCity.setName(rset.getString("Name"));
-//                    capitalCity.setContinent(rset.getString("Continent"));
-//                    capitalCity.setRegion(rset.getString("Region"));
                     capitalCity.setPopulation(rset.getInt("Population"));
-//                    capitalCity.setCapital(rset.getString("Capital"));
 
                     allCapitalCities.add(capitalCity);
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
-                System.out.println("Failed to retrieve country details");
+                System.out.println("Failed to retrieve capital city details");
             }
 
             return allCapitalCities;
         }
     }
+
+    //list all capital cities in a continent
+    public static List<City> getCapitalCitiesInContinent(Connection con, String continent, List<City> cities) {
+        List<City> capitalCitiesInContinent = new ArrayList<>();
+
+        try {
+
+            // Creates an SQL statement.
+            Statement stmt = con.createStatement();
+
+            // Sends the SQL statement to the database.
+            ResultSet rset = stmt.executeQuery(
+                        "SELECT city.Name, country.Name AS Country, city.Population "
+                                + "FROM country country "
+                                + "LEFT JOIN city city ON country.Capital = city.ID "
+                                + "WHERE country.Continent = '"+ continent +"' "
+                                + "ORDER BY city.Population DESC ");
+
+            while (rset.next()) {
+                //map query result to capitalCity object and add to list
+                City capitalCity = new City();
+
+                capitalCity.setName(rset.getString("Name"));
+                capitalCity.setPopulation(rset.getInt("Population"));
+
+                capitalCitiesInContinent.add(capitalCity);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to retrieve capital city details");
+        }
+        return capitalCitiesInContinent;
+    }
+
     //method to print a report from a list
     public static void printReport(String header, String format, List<City> list) {
         System.out.println(String.format(header));
 
         System.out.println(String.format(format,
-                "Code", "Name", "Continent", "Region", "Population", "Capital"));
+                "Name", "Population"));
 
-        // Loop over all countries in the list
+        // Loop over all capital cities in the list
         for (City capital : list)
         {
             if (capital == null) {
-                System.out.println("Country is null");
+                System.out.println("Capital City is null");
                 continue;
             }
 
             System.out.println(String.format(format,
-                    capital.getCountryCode(),
                     capital.getName(),
-//                    capital.getContinent(),
-//                    capital.getRegion(),
                     capital.getPopulation()));
-//                    capital.getCapital()));
         }
     }
 }
