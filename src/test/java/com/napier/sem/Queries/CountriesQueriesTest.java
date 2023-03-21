c6a3522d1ac1e86e7f39db0e119b57542cf22c44
Tiphaine <tiphainemorganemarie@gmail.com>
2023-03-21 09:41:39 +0000

    add unit tests to cover failure cases, update variable names

--- src/test/java/com/napier/sem/Queries/CountriesQueriesTest.java
+++ src/test/java/com/napier/sem/Queries/CountriesQueriesTest.java
@@ -1,9 +1,7 @@
 package com.napier.sem.Queries;
 
 import com.napier.sem.Country;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -16,16 +14,16 @@ public class CountriesQueriesTest {
     //the list of all the countries (mock data)
     List<Country> countries = MockData.getAllCountries();
 
-    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private static final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
-    @BeforeAll
-    public static void setUpStreams() {
+    @BeforeEach
+    public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
     }
 
-    @AfterAll
-    public static void restoreStreams() {
+    @AfterEach
+    public void restoreStreams() {
         System.setOut(originalOut);
     }
 
@@ -33,14 +31,14 @@ public class CountriesQueriesTest {
     @Test
     void expectContinentsToEqualMockData() {
         assert(CountriesQueries.getCountriesInContinent("Continent2", countries))
-                .equals(MockData.getAllContinentsMock());
+                .equals(MockData.getSingleContinentMock());
     }
 
     //assert that the method getCountriesInRegion() will equal the mock data
     @Test
     void expectRegionsToEqualMockData() {
         assert(CountriesQueries.getCountriesInRegion("Region2", countries))
-                .equals(MockData.getAllRegionsMock());
+                .equals(MockData.getSingleRegionMock());
     }
 
     //assert that the method getCountriesLimitedBy() will equal the mock data
@@ -61,5 +59,21 @@ public class CountriesQueriesTest {
                 "DEF        Country2                                           Continent1           Region1                                  10              Capital2       \n" +
                 "JKL        Country4                                           Continent2           Region2                                  5               Capital4       \n";
         assertEquals(dataString, outContent.toString());
+    }
+
+    // check that if a subset of countries is provided that doesn't exist, an empty report will be provided
+    @Test
+    void expectReportToBeBlank() {
+        CountriesQueries.printReport("Header", "%-10s %-50s %-20s %-40s %-15s %-15s", CountriesQueries.getCountriesInContinent("Random", this.countries));
+        String dataString = "Header\n" +
+                "Code       Name                                               Continent            Region                                   Population      Capital        \n";
+        assertEquals(dataString, outContent.toString());
+    }
+
+    // check that if the limit provided is larger than the subset, the subset will be returned
+    @Test
+    void expectTopContinentsWithHighLimitToEqualMockData() {
+        assert(CountriesQueries.getCountriesLimitedBy(10, MockData.getSingleContinentMock()))
+                .equals(MockData.getSingleContinentMock());
     }
 }
