38f6187e5d0fb205484e16c0e37e511a2e3d3b88
Max <mxmivlev@gmail.com>
2023-04-24 11:52:02 +0100

    Refactoring comments in Population.java

--- src/main/java/com/napier/sem/Population.java
+++ src/main/java/com/napier/sem/Population.java
@@ -1,6 +1,11 @@
 package com.napier.sem;
-
-public class Population { // constructor
+/**
+ * Populaton class
+ */
+public class Population {
+    /**
+     * Default constructor
+     */
     public Population() {
     }
 
@@ -13,46 +18,77 @@ public class Population { // constructor
         this.livingInCitiesPercentage = livingInCitiesPercentage;
         this.livingOutsidePercentage = livingOutsidePercentage;
     }
+    /**
+     * linvingInCities value
+     */
     private long livingInCities;
+    /**
+     *livingOutSideCitiesValue
+     */
     private long livingOutside;
+    /**
+     *livingInCitiesPercentage
+     */
     private double livingInCitiesPercentage;
+    /**
+     *livingOutsideCitiesPercentage
+     */
     private double livingOutsidePercentage;
 
-    // Population's name
+    /**
+     * Population's name
+     */
     private String name;
 
-    // Population County
+    /**
+     * Population County
+     */
     private long populationCountry;
 
-    // People living in cities
+    /**
+     * People living in cities
+     */
     private long populationCity;
 
-    // Updates the Population Name
+    /**
+     * Updates the Population Name
+     */
+
     public void setName(String name) {
         this.name = name;
     }
 
-    //gets the Population Name
+    /**
+     * gets the Population Name
+     */
     public String getName() {
         return name;
     }
 
-    // Updates the Population of people living in Countries
+    /**
+     * Updates the Population of people living in Countries
+     */
     public void setCountryPopulation(long populationCountry) {
         this.populationCountry = populationCountry;
     }
 
-    //gets the Population of people living in Countries
+    /**
+     * gets the Population of people living in Countries
+     */
     public long getCountryPopulation() {
         return populationCountry;
     }
 
-    // Updates the Population of people living in Cities
+    /**
+     * Updates the Population of people living in Cities
+     */
     public void setCityPopulation(long populationCity) {
         this.populationCity = populationCity;
     }
 
-    //gets the Population of people living in Cities
+    /**
+     * gets the Population of people living in Cities
+     */
     public long getCityPopulation() {
         return populationCity;
     }
