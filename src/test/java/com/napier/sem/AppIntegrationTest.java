8af5cc36daaff3cc42d0192dcb30501a4ee1aad0
Tiphaine <tiphainemorganemarie@gmail.com>
2023-04-25 10:20:38 +0100

    [wip] refactor, add more tests

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -3,16 +3,9 @@ package com.napier.sem;
 import com.napier.sem.Queries.CountriesQueries;
 import com.napier.sem.Queries.LanguagesQuery;
 import com.napier.sem.Queries.MockData;
-import com.napier.sem.Queries.Shared;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,23 +14,6 @@ public class AppIntegrationTest
 {
     static App app = new App();
     Connection con = app.connect("localhost:33060");
-    Statement statement = Shared.CreateStatement(con);
-
-    String simpleQuery = "SELECT country.Code AS 'Code'," +
-            "country.Name AS 'Name'," +
-            "country.Continent AS 'Continent'," +
-            "country.Region AS 'Region'," +
-            "country.Population AS 'Population'," +
-            "country.Capital AS 'Capital' " +
-            "FROM country " +
-            "GROUP BY country.Code " +
-            "ORDER BY country.Population DESC ";
-
-//    @Test
-//    void assertErrorOnFailedStatement() {
-//        ResultSet rset = CountriesQueries.getSqlResults(statement, simpleQuery);
-//        assertEquals(" ", rset.getString(1));
-//    }
 
     @Test
     void testCountryIsEqual() {
